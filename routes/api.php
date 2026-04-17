fb5a9acd8716054053b775b681c97a365bd77b22
Luis Garcia <lujogarpin78@gmail.com>
2026-04-17 06:29:22 -0500

    nuevos modulos contables

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,39 @@ Route::post('/logs/{id}/retry', [\App\Http\Controllers\SystemLogController::clas
 
 // (Asegurado) Endpoint POST agregado para reintentos de auditoría
 
+
+use App\Http\Controllers\ContableImportController;
+use App\Http\Controllers\ContableController;
+
+Route::prefix('contable')->group(function () {
+    Route::post('/upload/{type}', [ContableImportController::class, 'upload']);
+    Route::get('/facturas', [ContableController::class, 'getFacturas']);
+    Route::get('/bancos', [ContableController::class, 'getBancos']);
+    Route::get('/auxiliar', [ContableController::class, 'getAuxiliares']);
+    Route::get('/gastos', [ContableController::class, 'getGastos']);
+    Route::get('/imports', [ContableController::class, 'getImports']);
+    Route::delete('/clear', [ContableController::class, 'clearAll']);
+    Route::post('/reconcile', [App\Http\Controllers\ReconciliationController::class, 'reconcile']);
+});
+
+use App\Http\Controllers\PlanillaController;
+
+Route::prefix('planilla')->group(function () {
+    Route::get('/fincas', [PlanillaController::class, 'getFincas']);
+    Route::post('/fincas', [PlanillaController::class, 'storeFinca']);
+    
+    Route::get('/trabajadores', [PlanillaController::class, 'getTrabajadores']);
+    Route::post('/trabajadores', [PlanillaController::class, 'storeTrabajador']);
+    
+    Route::get('/labores', [PlanillaController::class, 'getLabores']);
+    Route::post('/labores', [PlanillaController::class, 'storeLabor']);
+    
+    Route::get('/actividades', [PlanillaController::class, 'getActividades']);
+    Route::post('/actividades', [PlanillaController::class, 'storeActividad']);
+    Route::delete('/actividades/{id}', [PlanillaController::class, 'deleteActividad']);
+    
+    Route::get('/gastos', [PlanillaController::class, 'getGastos']);
+    Route::post('/gastos', [PlanillaController::class, 'storeGasto']);
+    
+    Route::get('/resumen', [PlanillaController::class, 'getResumen']);
+});
